37b48caac4eb0a10550cd57d46acbb6e37effddf
michael-loh <54383090+michael-loh@users.noreply.github.com>
2019-12-31 16:19:11 -0500

    Update LittleSearchEngine.java

--- LittleSearchEngine.java
+++ LittleSearchEngine.java
@@ -41,10 +41,38 @@ public class LittleSearchEngine {
 	public HashMap<String,Occurrence> loadKeywordsFromDocument(String docFile) 
 	throws FileNotFoundException {
 		/** COMPLETE THIS METHOD **/
+		if(docFile == null){
+			return null;
+		}
 		
-		// following line is a placeholder to make the program compile
-		// you should modify it as needed when you write your code
-		return null;
+		File file = new File(docFile);
+		Scanner sc = new Scanner(file);
+		
+		HashMap<String, Occurrence> hMap = new HashMap<String, Occurrence>();
+		
+		//goes through the file, one line at a time
+		while(sc.hasNext()){
+			
+			String word = sc.next();
+			
+			String keyWord = getKeyword(word);
+			if(keyWord != null){
+				
+				if(!hMap.containsKey(keyWord)){
+					Occurrence temp = new Occurrence(docFile, 1);
+					hMap.put(keyWord, temp);
+				}
+				
+				else{
+					hMap.get(keyWord).frequency++;
+				}
+				
+			}
+			
+		}
+		
+		
+		return hMap;
 	}
 	
 	/**
@@ -58,6 +86,30 @@ public class LittleSearchEngine {
 	 */
 	public void mergeKeywords(HashMap<String,Occurrence> kws) {
 		/** COMPLETE THIS METHOD **/
+		Set setOfKeys = kws.keySet();
+		
+		
+		Iterator iterator = setOfKeys.iterator();
+		
+		
+		while(iterator.hasNext()){
+			
+			String key = (String) iterator.next();
+			
+			if(keywordsIndex.containsKey(key)){
+				keywordsIndex.get(key).add(kws.get(key));
+				insertLastOccurrence(keywordsIndex.get(key));
+			}
+			else if(!keywordsIndex.containsKey(key)){
+				ArrayList<Occurrence> occs = new ArrayList<Occurrence>();
+				occs.add(kws.get(key));
+				keywordsIndex.put(key, occs);
+				insertLastOccurrence(keywordsIndex.get(key));
+			}
+			
+			
+		}
+		
 	}
 	
 	/**
@@ -71,12 +123,37 @@ public class LittleSearchEngine {
 	 * @param word Candidate word
 	 * @return Keyword (word without trailing punctuation, LOWER CASE)
 	 */
+	private boolean isPunctuation(char a){
+		if(a == '.' || a == ',' || a =='?' || a == ':' || a == ';' || a == '!'){
+			return true;
+		}
+		return false;
+	}
+	
 	public String getKeyword(String word) {
 		/** COMPLETE THIS METHOD **/
+		String editedWord = "";
 		
-		// following line is a placeholder to make the program compile
-		// you should modify it as needed when you write your code
-		return null;
+		if(isPunctuation(word.charAt(word.length()-1))){
+			editedWord = word.substring(0, word.length()-1);
+		}
+		else{
+			editedWord = word;
+		}
+		
+		editedWord = editedWord.toLowerCase();
+		
+		for(int i = 0; i < editedWord.length(); i++){
+			if(!Character.isLetter(editedWord.charAt(i))){
+				return null;
+			}
+		}
+		
+		if(noiseWords.contains(editedWord)){
+			return null;
+		}
+		
+		return editedWord;
 	}
 	
 	/**
@@ -92,10 +169,70 @@ public class LittleSearchEngine {
 	 */
 	public ArrayList<Integer> insertLastOccurrence(ArrayList<Occurrence> occs) {
 		/** COMPLETE THIS METHOD **/
+		ArrayList<Integer> indexes = new ArrayList<Integer>();
 		
+		if(occs.size() == 0){
+			return null;
+		}
+		
+		if(occs.size() == 1){
+			
+			indexes.add(0);
+			return indexes;
+		}
+		
+		int left = 0;
+		int right = occs.size()-2;
+		int mid = 0;
+		int occMid = occs.get(mid).frequency;
+		String doc = occs.get(occs.size()-1).document;
+		int frequency = occs.get(occs.size()-1).frequency;
+		
+		while(left <= right){
+			
+			
+			mid = (right + left)/2;
+			occMid = occs.get(mid).frequency;
+			indexes.add(occMid);
+			
+			
+			if(frequency == occMid)
+				break;
+			else if(frequency < occMid){
+				left = mid + 1;
+			}
+			else if(frequency > occMid){
+				right = mid - 1;
+			}
+			
+		}
+		
+		int temp = 0;
+		
+		if(occs.size()==2){
+			if(occs.get(0).frequency < occs.get(1).frequency){
+				String tempOccString = occs.get(0).document;
+				int tempOccFreq = occs.get(0).frequency;
+				occs.get(0).frequency = occs.get(1).frequency;
+				occs.get(0).document = occs.get(1).document;
+				occs.get(1).document = tempOccString;
+				occs.get(1).frequency = tempOccFreq;
+			}
+		}
+		
+		else{
+			for(int i = occs.size()-1; i > mid; i--){
+				Occurrence t2 = occs.get(i-1);
+				occs.get(i).document = t2.document;
+				occs.get(i).frequency = t2.frequency;
+			}
+		
+			occs.get(mid).document = doc;
+			occs.get(mid).frequency = frequency;
+		}
 		// following line is a placeholder to make the program compile
 		// you should modify it as needed when you write your code
-		return null;
+		return indexes;
 	}
 	
 	/**
@@ -136,16 +273,69 @@ public class LittleSearchEngine {
 	 * The result set is limited to 5 entries. If there are no matches at all, result is null.
 	 * 
 	 * @param kw1 First keyword
-	 * @param kw1 Second keyword
+	 * @param kw2 Second keyword
 	 * @return List of documents in which either kw1 or kw2 occurs, arranged in descending order of
 	 *         frequencies. The result size is limited to 5 documents. If there are no matches, returns null.
 	 */
 	public ArrayList<String> top5search(String kw1, String kw2) {
 		/** COMPLETE THIS METHOD **/
 		
+		int kw1Size = 0;
+		int kw2Size = 0;
+		
+		if(!keywordsIndex.containsKey(kw1)){
+			kw1Size = keywordsIndex.get(kw1).size();
+		}
+		
+		if(!keywordsIndex.containsKey(kw2)){
+			kw2Size = keywordsIndex.get(kw2).size();
+		}
+		
+		if(kw1Size >= 5){
+			kw1Size = 5;
+		}
+		
+		if(kw2Size >= 5){
+			kw2Size = 5;
+		}
+		
+		ArrayList<String>documents = null;
+		int freq1 = 0;
+		int freq2 = 0;
+		if(keywordsIndex.containsKey(kw1) && keywordsIndex.containsKey(kw2)){
+			
+			int index1 = 0;
+			int index2 = 0;
+			
+			while(index1 < kw1Size || index2 < kw2Size){
+				//if we haven't traversed through both array lists yet
+				if(index1 < kw1Size && index2 < kw2Size){
+					if(keywordsIndex.get(kw1).get(index1).frequency >= keywordsIndex.get(kw2).get(index2).frequency){
+						documents.add(keywordsIndex.get(index1).get(index1).document);
+						index1++;
+					}
+					else{
+						documents.add(keywordsIndex.get(index2).get(index2).document);
+						index2++;
+					}
+				}
+				else if(index1 < kw1Size){
+					documents.add(keywordsIndex.get(kw1).get(index1).document);
+					index1++;
+				}
+				else if(index1 > kw2Size){
+					documents.add(keywordsIndex.get(kw2).get(index2).document);
+					index2++;
+				}
+				
+			}
+			
+			
+		}
+		
 		// following line is a placeholder to make the program compile
 		// you should modify it as needed when you write your code
-		return null;
+		return documents;
 	
 	}
 }
